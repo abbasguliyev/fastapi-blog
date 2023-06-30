967cdf04dfcf7bc9a44d7d7eaad167c81af880ad
abbasguliyev <abbasquliyev111@gmail.com>
2023-06-30 08:04:32 +0400

    test

--- src/main.py
+++ src/main.py
@@ -5,5 +5,5 @@ from .models import blog as blog_model, user as user_model
 
 app = FastAPI()
 
-app.include_router(user_router.router, prefix="/user")
 app.include_router(blog_router.router, prefix="/blogs")
+app.include_router(user_router.router, prefix="/user")
